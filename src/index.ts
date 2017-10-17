2495dff539136bf5df61410aebd28fa1747f21e9
dvpetrov <dvpetrov.main@gmail.com>
2017-10-17 13:07:36 +0300

    Add TSLint config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default } from './ConfigPluginApi'
 export {
   default as createConfigActivator,
-  ConfigPlugin
+  ConfigPlugin,
 } from './ConfigPlugin'
 export { default as ConfigPluginConstructor } from './ConfigPluginConstructor'
